5d4235450d5bc97f9865a624b5c55d1be7a21ce0
Missbellum <enjaykanz@gmail.com>
2025-10-23 16:05:56 +0100

    Modified /Blog styling for mobile 04

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -27,7 +27,7 @@ export default function LearnPage() {
 
           {/* Animated color fade-in */}
           <Motion.span
-            className="inline-block"
+            className="px-30 sm:px-0 inline-block"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.4 }}

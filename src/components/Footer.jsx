67151f46ec976f2ec239e58c8ab820040fe042b5
Missbellum <enjaykanz@gmail.com>
2026-02-04 11:42:42 +0100

    Minor footer tweak

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,7 +39,7 @@ export default function Footer() {
         {/* Brand Column */}
         <div>
           <h3 className="text-lime-400 text-xl font-bold mb-3">
-            VeryCodedly<span className="text-xs">™</span>
+            VeryCodedly{/* <span className="text-xs">™</span> */}
           </h3>
           <p className="text-sm text-gray-400/80 leading-tight w-[140px]">
             For the curious minds shaping the future, you're in good company here.

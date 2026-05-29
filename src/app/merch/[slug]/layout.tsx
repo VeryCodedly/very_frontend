6cda290421ecc8aac643eadfc4fdad287331e2c8
Missbellum <enjaykanz@gmail.com>
2026-05-29 22:25:17 +0100

    Added meta fot store item

--- src/app/merch/[slug]/layout.tsx
+++ src/app/merch/[slug]/layout.tsx
@@ -51,6 +51,13 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
         { url: product.preview_image || 'https://verycodedly.com/og-image.png' },
       ],
     },
+    twitter: {
+      card: "summary_large_image",
+      title: "Merch",
+      description: product.tagline || `Make the ${product.fancy_name} look even better.`,
+      images: ["https://verycodedly.com/twitter-image.png"],
+      creator: '@verycodedly'
+    },
   };
 }
 
